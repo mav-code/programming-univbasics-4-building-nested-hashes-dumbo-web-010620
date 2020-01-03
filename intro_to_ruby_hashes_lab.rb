2e36bcc23cb3e9a87a604e90f16f833c45285387
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-03 21:18:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,8 +25,8 @@ def monopoly_with_third_tier
      :names => {
        :reading_railroad => {},
        :pennsylvania_railroad => {},
-       :short_line_railroad => {},
-       :b_and_o_railroad => {},
+       :shortline_railroad => {},
+       :bando_railroad => {},
    }
      }
    }
