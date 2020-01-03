b4102c98fa7df07c2853396dbdbe6700a3eebaa4
Mavi Toktamis-Paker <mavisansmembres@gmail.com>
2020-01-03 21:14:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,9 +18,9 @@ def monopoly_with_third_tier
      :pieces => 4,
      :rent_in_dollars => {
        :one_piece_owned => 25,
-       :two_pieces_owned => "something else",
-       :three_pieces_owned => "a third thing",
-       :four_pieces_owned => "the last thing",
+       :two_pieces_owned => 50,
+       :three_pieces_owned => 75,
+       :four_pieces_owned => 100,
      },
      :names => {
        :key1 => "something",
